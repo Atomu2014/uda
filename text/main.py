a3462e091016792369d01f00ff78c91ac2766d39
Atomu2014 <kevinqu16@gmail.com>
2019-11-13 16:15:37 -0600

    test tpu

--- text/main.py
+++ text/main.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import json
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 import uda
--- text/scripts/train_large_ft_tpu.sh
+++ text/scripts/train_large_ft_tpu.sh
@@ -12,37 +12,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-train_tpu=node-1
-eval_tpu=node-2
-model_dir=gs://qizhex/uda/text/ckpt/large_ft_exp_1
+train_tpu=kevin
+eval_tpu=kevin
+model_dir=$GS/uda/text/ckpt/large_ft_exp_1
 
-python main.py \
+python3 main.py \
   --use_tpu=True \
   --tpu_name=${train_tpu} \
   --do_train=True \
   --do_eval=False \
-  --sup_train_data_dir= \
-  --eval_data_dir= \
-  --bert_config_file= \
-  --vocab_file= \
-  --init_checkpoint= \
+  --sup_train_data_dir=$GS/uda/text/data/proc_data/IMDB/train_20 \
+  --eval_data_dir=$GS/uda/text/data/proc_data/IMDB/dev \
+  --bert_config_file=$GS/uda/text/pretrained_models/bert_base/bert_config.json \
+  --vocab_file=$GS/uda/text/pretrained_models/bert_base/vocab.txt \
+  --init_checkpoint=$GS/uda/text/pretrained_models/bert_base/bert_model.ckpt \
   --task_name=IMDB \
-  --model_dir=gs://uda_data/UDA/ckpt/large_ft \
+  --model_dir=$GS/uda/text/ckpt/large_ft \
   --max_seq_length=512 \
   --num_train_steps=3000 \
   --learning_rate=3e-05 \
   --train_batch_size=32 \
   --num_warmup_steps=300
 
-python main.py \
+python3 main.py \
   --use_tpu=True \
   --tpu_name=${eval_tpu} \
   --do_train=False \
   --do_eval=True \
-  --sup_train_data_dir= \
-  --eval_data_dir= \
-  --bert_config_file= \
-  --vocab_file= \
+  --sup_train_data_dir=$GS/uda/text/data/proc_data/IMDB/train_20 \
+  --eval_data_dir=$GS/uda/text/data/proc_data/IMDB/dev \
+  --bert_config_file=$GS/uda/text/pretrained_models/bert_base/bert_config.json \
+  --vocab_file=$GS/uda/text/pretrained_models/bert_base/vocab.txt \
   --task_name=IMDB \
   --model_dir=${model_dir} \
   --max_seq_length=512 \
