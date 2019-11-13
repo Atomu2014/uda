e9e6a49b18d4989cd3fc840328f1b30a3bf09189
Atomu2014 <kevinqu16@gmail.com>
2019-11-13 15:04:54 -0600

    minor

--- back_translate/run.sh
+++ back_translate/run.sh
@@ -74,24 +74,23 @@ mkdir -p ${para_dir}
 #  --use_tpu \
 #  --cloud_tpu_name=$TPU_NAME
 
-echo "*** backward translation ***"
-t2t-decoder \
-  --problem=translate_enfr_wmt32k_rev \
-  --model=transformer \
-  --hparams_set=transformer_big \
-  --hparams="sampling_method=random,sampling_temp=${sampling_temp}" \
-  --decode_hparams="beam_size=1,batch_size=16,alpha=0" \
-  --checkpoint_path=$GS/uda/back_translate/checkpoints/fren/model.ckpt-500000 \
-  --output_dir=/tmp/t2t \
-  --decode_from_file=$GS/uda/back_translate/${forward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
-  --decode_to_file=$GS/uda/back_translate/${backward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
-  --data_dir=$GS/uda/back_translate/checkpoints \
-  --use_tpu \
-  --cloud_tpu_name=$TPU_NAME
+#echo "*** backward translation ***"
+#t2t-decoder \
+#  --problem=translate_enfr_wmt32k_rev \
+#  --model=transformer \
+#  --hparams_set=transformer_big \
+#  --hparams="sampling_method=random,sampling_temp=${sampling_temp}" \
+#  --decode_hparams="beam_size=1,batch_size=16,alpha=0" \
+#  --checkpoint_path=$GS/uda/back_translate/checkpoints/fren/model.ckpt-500000 \
+#  --output_dir=/tmp/t2t \
+#  --decode_from_file=$GS/uda/back_translate/${forward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
+#  --decode_to_file=$GS/uda/back_translate/${backward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
+#  --data_dir=$GS/uda/back_translate/checkpoints \
+#  --use_tpu \
+#  --cloud_tpu_name=$TPU_NAME
 
-#echo "*** transform sentences back into paragraphs***"
-#python3 sent_to_paragraph.py \
-#  --input_file=${backward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
-#  --doc_len_file=${doc_len_dir}/doc_len_${worker_id}_of_${replicas}.json \
-#  --output_file=${para_dir}/file_${worker_id}_of_${replicas}.json
-#
+echo "*** transform sentences back into paragraphs***"
+python3 sent_to_paragraph.py \
+  --input_file=$GS/uda/back_translate${backward_gen_dir}/file_${worker_id}_of_${replicas}.txt \
+  --doc_len_file=$GS/uda/back_translate${doc_len_dir}/doc_len_${worker_id}_of_${replicas}.json \
+  --output_file=$GS/uda/back_translate${para_dir}/file_${worker_id}_of_${replicas}.json
