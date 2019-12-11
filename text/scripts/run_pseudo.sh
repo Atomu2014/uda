7243ea315a6e1d04d7bc65aa9b5d834b9afd62e7
Atomu2014 <kevinqu16@gmail.com>
2019-12-11 02:39:08 -0600

    minor

--- text/scripts/run_pseudo.sh
+++ text/scripts/run_pseudo.sh
@@ -1,16 +1,22 @@
+data=bt-1
+ckpt=bt_1_0
+steps=30000
+warmup=3000
+lr=1e-5
+
 python3 main.py \
   --use_tpu=False \
   --tpu_name=kevin \
   --do_train=True \
   --do_eval=True \
-  --sup_train_data_dir=$GS/uda/text/data/proc_data/IMDB/pseudo/bt-0.9/0 \
+  --sup_train_data_dir=$GS/uda/text/data/proc_data/IMDB/pseudo/${data}/0 \
   --eval_data_dir=$GS/uda/text/data/proc_data/IMDB/dev \
   --bert_config_file=$GS/uda/text/pretrained_models/bert_base/bert_config.json \
   --vocab_file=$GS/uda/text/pretrained_models/bert_base/vocab.txt \
   --init_checkpoint=$GS/uda/text/pretrained_models/bert_base/bert_model.ckpt \
   --task_name=IMDB \
-  --model_dir=$GS/uda/text/ckpt/bt_0.9_0 \
-  --num_train_steps=30000 \
-  --learning_rate=1e-05 \
-  --num_warmup_steps=3000 \
+  --model_dir=$GS/uda/text/ckpt/${ckpt} \
+  --num_train_steps=${steps} \
+  --learning_rate=${lr} \
+  --num_warmup_steps=${warmup} \
   --max_seq_length=${MAX_SEQ_LENGTH}
