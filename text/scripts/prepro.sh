4e2573f0526beaf8ad228482873748d64d2e36a9
Atomu2014 <kevinqu16@gmail.com>
2019-12-11 01:46:06 -0600

    minor

--- text/scripts/prepro.sh
+++ text/scripts/prepro.sh
@@ -16,11 +16,23 @@
 bert_vocab_file=pretrained_models/bert_base/vocab.txt
 
 
+#python3 preprocess.py \
+#  --raw_data_dir=data/IMDB_raw/csv/train.csv \
+#  --output_base_dir=../back_translate/imdb.txt \
+#  --data_type=back_trans \
+#  --vocab_file=$bert_vocab_file
+
+
 python3 preprocess.py \
-  --raw_data_dir=data/IMDB_raw/csv/train.csv \
-  --output_base_dir=../back_translate/imdb.txt \
-  --data_type=back_trans \
-  --vocab_file=$bert_vocab_file
+  --raw_data_dir=data/IMDB_raw/csv \
+  --output_base_dir=data/proc_data/IMDB/pseudo \
+  --back_translation_dir=data/back_translation/imdb_back_trans \
+  --data_type=pseudo \
+  --sub_set=unsup_in \
+  --aug_ops=bt-1 \
+  --aug_copy_num=0 \
+  --vocab_file=$bert_vocab_file \
+  --max_seq_length=${MAX_SEQ_LENGTH}
 
 
 ## Preprocess supervised training set
