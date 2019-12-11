6a544517cfc4b1e9af974c0b0fdc1974c8799166
Atomu2014 <kevinqu16@gmail.com>
2019-12-11 02:27:50 -0600

    minor

--- text/scripts/run_pseudo.sh
+++ text/scripts/run_pseudo.sh
@@ -1,17 +1,3 @@
-# coding=utf-8
-# Copyright 2019 The Google UDA Team Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 python3 main.py \
   --use_tpu=False \
   --tpu_name=kevin \
@@ -23,8 +9,8 @@ python3 main.py \
   --vocab_file=$GS/uda/text/pretrained_models/bert_base/vocab.txt \
   --init_checkpoint=$GS/uda/text/pretrained_models/bert_base/bert_model.ckpt \
   --task_name=IMDB \
-  --model_dir=$GS/uda/text/ckpt/pseudo \
-  --num_train_steps=3000 \
-  --learning_rate=3e-05 \
-  --num_warmup_steps=300 \
+  --model_dir=$GS/uda/text/ckpt/bt_0.9_0 \
+  --num_train_steps=30000 \
+  --learning_rate=1e-05 \
+  --num_warmup_steps=3000 \
   --max_seq_length=${MAX_SEQ_LENGTH}
