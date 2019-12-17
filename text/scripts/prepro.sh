cdec54099e812cc99ae4558d5ebb48732da6a633
Atomu2014 <kevinqu16@gmail.com>
2019-12-16 23:23:23 -0600

    add yelp-2

--- text/scripts/prepro.sh
+++ text/scripts/prepro.sh
@@ -1,17 +1,3 @@
-# coding=utf-8
-# Copyright 2019 The Google UDA Team Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #!/bin/bash
 bert_vocab_file=pretrained_models/bert_base/vocab.txt
 
@@ -23,16 +9,16 @@ bert_vocab_file=pretrained_models/bert_base/vocab.txt
 #  --vocab_file=$bert_vocab_file
 
 
-python3 preprocess.py \
-  --raw_data_dir=data/IMDB_raw/csv \
-  --output_base_dir=data/proc_data/IMDB/pseudo \
-  --back_translation_dir=data/back_translation/imdb_back_trans \
-  --data_type=pseudo \
-  --sub_set=unsup_in \
-  --aug_ops=bt-1 \
-  --aug_copy_num=0 \
-  --vocab_file=$bert_vocab_file \
-  --max_seq_length=${MAX_SEQ_LENGTH}
+#python3 preprocess.py \
+#  --raw_data_dir=data/IMDB_raw/csv \
+#  --output_base_dir=data/proc_data/IMDB/pseudo \
+#  --back_translation_dir=data/back_translation/imdb_back_trans \
+#  --data_type=pseudo \
+#  --sub_set=unsup_in \
+#  --aug_ops=bt-1 \
+#  --aug_copy_num=0 \
+#  --vocab_file=$bert_vocab_file \
+#  --max_seq_length=${MAX_SEQ_LENGTH}
 
 
 ## Preprocess supervised training set
@@ -44,26 +30,28 @@ python3 preprocess.py \
 #  --sup_size=20 \
 #  --vocab_file=$bert_vocab_file \
 #  --max_seq_length=${MAX_SEQ_LENGTH}
-#
-#python3 preprocess.py \
-#  --raw_data_dir=data/IMDB_raw/csv \
-#  --output_base_dir=data/proc_data/IMDB/train \
-#  --data_type=sup \
-#  --sub_set=train \
-#  --sup_size=-1 \
-#  --vocab_file=$bert_vocab_file \
-#  --max_seq_length=${MAX_SEQ_LENGTH}
-#
-## Preprocess test set
-#python3 preprocess.py \
-#  --raw_data_dir=data/IMDB_raw/csv \
-#  --output_base_dir=data/proc_data/IMDB/dev \
-#  --data_type=sup \
-#  --sub_set=dev \
-#  --vocab_file=$bert_vocab_file \
-#  --max_seq_length=${MAX_SEQ_LENGTH}
-#
-#
+
+python3 preprocess.py \
+  --task_name=yelp-2 \
+  --raw_data_dir=data/yelp-2 \
+  --output_base_dir=data/proc_data/yelp-2/train \
+  --data_type=sup \
+  --sub_set=train \
+  --sup_size=-1 \
+  --vocab_file=$bert_vocab_file \
+  --max_seq_length=${MAX_SEQ_LENGTH}
+
+# Preprocess test set
+python3 preprocess.py \
+  --task_name=yelp-2 \
+  --raw_data_dir=data/yelp-2 \
+  --output_base_dir=data/proc_data/yelp-2/dev \
+  --data_type=sup \
+  --sub_set=dev \
+  --vocab_file=$bert_vocab_file \
+  --max_seq_length=${MAX_SEQ_LENGTH}
+
+
 ## Preprocess unlabeled set
 #python3 preprocess.py \
 #  --raw_data_dir=data/IMDB_raw/csv \
